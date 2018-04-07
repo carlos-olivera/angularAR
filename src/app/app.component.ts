1f62071bea39b617e9ad65c297f587b578f8e519
Carlos Olivera <carlos.olivera.t@gmail.com>
2018-04-06 21:21:20 -0400

    initial demo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'a-root',
   template: `
-    <a-pattern-marker model='sphere'></a-pattern-marker>
+    <a-pattern-marker model='torus'></a-pattern-marker>
   `,
   styleUrls: ['./app.component.scss']
 })
